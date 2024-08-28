02f93014b9a9892d2bf730382694150488b47e29
Alexandre Desoutter <alexd@MacBook-Pro-de-Alexandre.local>
2024-08-28 19:00:09 +0200

    db config

--- app/controller/playerController.js
+++ app/controller/playerController.js
@@ -3,9 +3,8 @@ const { findRanking } = require('../model/playerModel');
 // Display all players
 const getRanking = async (req, res) => {
 
-    res.status(200).json({ data: "players" });
-    // let players = await findRanking();
-    // res.status(200).json({ data: players });
+    let players = await findRanking();
+    res.status(200).json({ data: players });
 };
 
 module.exports = {
