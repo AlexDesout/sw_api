7948b17ab2b13d0b87eecb81955f5613db27c3ff
Alexandre Desoutter <alexd@MacBook-Pro-de-Alexandre.local>
2024-08-28 16:51:43 +0200

    test

--- app/router/playerRoutes.js
+++ app/router/playerRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const playerController = require("../controller/playerController");
 const authenticateToken = require('../middleware/authMiddleware');
 
-// Liste des routes :
+// Liste des routes 💰:
 router.get("/ranking", authenticateToken, playerController.getRanking); // Top 10 best players
 
 
